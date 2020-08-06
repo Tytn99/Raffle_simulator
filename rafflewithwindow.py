6ad2782886ace7c05edeffbc3f419df7beebd8e7
Tyler Russell <57043826+Tytn99@users.noreply.github.com>
2020-08-06 12:01:25 -0500

    Update rafflewithwindow.py

--- rafflewithwindow.py
+++ rafflewithwindow.py
@@ -1,41 +1,45 @@
 from tkinter import *
 import random
+#font used - from google fonts dl to desktop for gui. link - https://fonts.google.com/specimen/Red+Rose
 
-Win = f'''Won - Invoice from __ #({random.randint(100, 4000)})
-Congrats on your w! check your email and you will see your invoice!!'''
-L = "Another L"
+Win = f'''Won - Invoice - Paypal #({random.randint(100, 4000)})
+Congrats on your W! check your email and you will see your invoice!!'''
+L = "ANOTHER L"
 chance = "You have a 0.625% chance of winning each time."
+wrong = "-Invalid entry-    Enter only numbers between 1 and 1600, then re-submit raffle"
 
 root = Tk()
 root.title("Raffler")
 root.configure(background="black")
 
-label = Label(root, text="ENTER RAFFLE", bg='black', fg='white', font=('rose', 18, 'bold'))
-label.grid(sticky=W)
+label = Label(root, text="ENTER RAFFLE", bg='black', fg='white', font=('Red Rose', 18, 'bold'))
+label.grid(row=1, column=0)
+
+label2 = Label(root, text="Enter number between 1 and 1600", bg='black', fg='#edbe00', font=('Red Rose', 14, 'bold'))
+label2.grid(row=2, column=0)
 
 def UserEntry():
     entered_text = textEntry.get()
     entries = list(range(1, 1600))
     numbers_generated = random.choices(entries, k=10)
     your_number = int(entered_text)
-    pulled = (f'resulting numbers : {numbers_generated}')
+    pulled = f'resulting numbers : {numbers_generated}'
     output.delete(0.0, END)
     if your_number in numbers_generated:
-        answer = (Win, pulled)
-    else:
-        answer = (L, pulled)
-    output.insert(END, answer)
+        answer = (Win + "  ,  " + pulled + " " + chance)
+    elif your_number > 1600:
+        answer = wrong
+    elif your_number not in numbers_generated:
+        answer = (L + " , " + pulled + " , " + "  " + chance)
+    output.insert(END, answer, chance)
 
-textEntry = Entry(root, width=20, bg="white")
-textEntry.grid(sticky=W)
+textEntry = Entry(root, width=20, bg="white", justify='center')
+textEntry.grid(row=3, column=0, padx=5, pady=5)
 
-thebutton = Button(root, command=UserEntry,text="RUN RAFFLE", font=('rose', 18, 'bold'), bg="#ffe600", fg="white")
-thebutton.grid(sticky=W)
+thebutton = Button(root, command=UserEntry, text="SUBMIT RAFFLE", font=('Red Rose', 12, 'bold'), bg="#edbe00", fg="white")
+thebutton.grid(row=6, column=0, padx=10, pady=10)
 
-label2 = Label(root, text="Result: ", bg='black', fg='white', font=('rose', 12, 'bold'))
-label2.grid(sticky=(W))
-
-output = Text(root, width=88, height=6, wrap=WORD, background="white")
-output.grid(row=5, column=0)
+output = Text(root, width=105, height=3, wrap=WORD, background="black", foreground="white", font=('Red Rose', 18))
+output.grid(row=7, column=0, sticky=W+E+N+S)
 
 root.mainloop()
